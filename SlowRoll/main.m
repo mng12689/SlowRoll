61353787e51a8d6a0d74e119b9545589eb6b72a7
mng12689 <mng12689@gmail.com>
2013-12-21 14:50:53 -0500

    initial commit

---
+++ SlowRoll/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlowRoll
+//
+//  Created by Michael Ng on 12/21/13.
+//  Copyright (c) 2013 SlowRoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRAppDelegate class]));
+    }
+}
---
+++ SlowRollTests/SlowRollTests.m
@@ -0,0 +1,34 @@
+//
+//  SlowRollTests.m
+//  SlowRollTests
+//
+//  Created by Michael Ng on 12/21/13.
+//  Copyright (c) 2013 SlowRoll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SlowRollTests : XCTestCase
+
+@end
+
+@implementation SlowRollTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
