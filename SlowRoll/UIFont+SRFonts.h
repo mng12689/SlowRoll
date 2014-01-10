13b371c64df3a7c97e9d0ab64ac7f71e2d312c5a
mng12689 <mng12689@gmail.com>
2014-01-10 12:01:27 -0500

    add category for official sr font

---
+++ SlowRoll/UIFont+SRFonts.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+SRFonts.h
+//  SlowRoll
+//
+//  Created by Michael Ng on 1/10/14.
+//  Copyright (c) 2014 SlowRoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (SRFonts)
+
++ (UIFont *)futuraFontWithSize:(CGFloat)size;
+
+@end
---
+++ SlowRoll/UIFont+SRFonts.m
@@ -0,0 +1,18 @@
+//
+//  UIFont+SRFonts.m
+//  SlowRoll
+//
+//  Created by Michael Ng on 1/10/14.
+//  Copyright (c) 2014 SlowRoll. All rights reserved.
+//
+
+#import "UIFont+SRFonts.h"
+
+@implementation UIFont (SRFonts)
+
++ (UIFont *)futuraFontWithSize:(CGFloat)size
+{
+    return [UIFont fontWithName:@"Futura" size:size];
+}
+
+@end
